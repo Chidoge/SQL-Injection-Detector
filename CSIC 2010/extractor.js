8100d7ada72b6effe75d052a725dcffd0eb73cf1
Lincoln <lincoln.choy1997@gmail.com>
2019-10-12 23:17:27 +1300

    Added tokenizer

--- CSIC 2010/extractor.js
+++ CSIC 2010/extractor.js
@@ -9,18 +9,19 @@ getRequest = (file) => {
         /* Find HTTP request */
         if (line.indexOf('GET') !== -1 || line.indexOf('POST') !== -1) {
 
+            line = line.split(' ')[1];
             /* Request needs to be SQL-related */
             if (line.indexOf('=') !== -1) {
 
                 if (file === "anomalousTrafficTest") {
                     if (line.toLowerCase().indexOf('where') !== -1 || line.toLowerCase().indexOf('select') !== -1 || line.toLowerCase().indexOf('drop') !== -1) {
-                        fs.appendFile(`${file}/requests.txt`, line + '\n', function (err) {
+                        fs.appendFileSync(`${file}/requests.txt`, line + '\n', function (err) {
                             if (err) throw err;
                         });
                     }
                 }
                 else {
-                    fs.appendFile(`${file}/requests.txt`, line + '\n', function (err) {
+                    fs.appendFileSync(`${file}/requests.txt`, line + '\n', function (err) {
                         if (err) throw err;
                     });
                 }
@@ -34,5 +35,47 @@ getRequest = (file) => {
 
 }
 
+tokenize = (file) => {
+    const lineReader = require('line-reader');
+
+    lineReader.eachLine(`${file}/requests.txt`, function(line, last) {
+
+        /* Find queries */
+        if (line.indexOf('?') !== -1) {
+
+            /* Find first query */
+            const index = line.indexOf('?');
+            let queryOnly = line.substring(index + 1, line.length);
+            let paramIndex;
+
+            /* Write token */
+            let onlyOneToken = true;
+            while ((paramIndex = queryOnly.indexOf('&')) !== -1) {
+                onlyOneToken = false;
+                /* Save each x=y token */
+                const newParam = queryOnly.substring(0, paramIndex);
+                queryOnly = queryOnly.substring(paramIndex + 1, queryOnly.length);
+                fs.appendFileSync(`${file}/tokens.txt`, newParam + ' ' , function (err) {
+                    if (err) throw err;
+                });
+            }
+            if (onlyOneToken) {
+                fs.appendFileSync(`${file}/tokens.txt`, queryOnly + ' ' , function (err) {
+                    if (err) throw err;
+                });
+            }
+            fs.appendFileSync(`${file}/tokens.txt`, '\n' , function (err) {
+                if (err) throw err;
+            });
+        }
+        if (last) {
+            console.log('Done');
+        }
+    });
+}
+
 // getRequest('normalTrafficTest');
-getRequest('anomalousTrafficTest');
+// getRequest('anomalousTrafficTest');
+
+// tokenize('normalTrafficTest');
+tokenize('anomalousTrafficTest');
